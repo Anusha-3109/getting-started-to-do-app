c9994d75222822b6b00f54cfd58f73e79345464c
Anusha <anusha.abburi@fielmann.com>
2026-03-26 14:33:57 +0100

    Fix syntax error in addItem route

--- backend/src/routes/addItem.js
+++ backend/src/routes/addItem.js
@@ -2,6 +2,7 @@ const db = require('../persistence');
 const { v4: uuid } = require('uuid');
 
 module.exports = async (req, res) => {
+    console.log("New item added");
     const item = {
         id: uuid(),
         name: req.body.name,
